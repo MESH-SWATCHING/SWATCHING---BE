b110f2fecebe936a3b45191c556053ac7a101c46
chamelewan <wani001110@gmail.com>
2026-06-27 15:16:04 +0900

    feat: Initial Spring Boot project setup

---
+++ src/main/java/com/swatching/swatching_be/SwatchingBeApplication.java
@@ -0,0 +1,13 @@
+package com.swatching.swatching_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwatchingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwatchingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swatching/swatching_be/SwatchingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swatching.swatching_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwatchingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
