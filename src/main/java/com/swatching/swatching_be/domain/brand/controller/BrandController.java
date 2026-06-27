09554660cf02e61c3c6b817e188a20f70930dade
chamelewan <wani001110@gmail.com>
2026-06-27 20:34:30 +0900

    feat: [HOME-05] 브랜드 목록 조회 Controller 구현

---
+++ src/main/java/com/swatching/swatching_be/domain/brand/controller/BrandController.java
@@ -0,0 +1,28 @@
+package com.swatching.swatching_be.domain.brand.controller;
+
+import com.swatching.swatching_be.domain.brand.dto.BrandResponseDto;
+import com.swatching.swatching_be.domain.brand.service.BrandService;
+import com.swatching.swatching_be.global.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class BrandController {
+
+    private final BrandService brandService;
+
+    @GetMapping("/brands")
+    public ResponseEntity<ApiResponse<List<BrandResponseDto>>> getBrands(
+            @RequestParam(required = false) String keywords) {
+        List<BrandResponseDto> data = brandService.getBrands(keywords);
+        return ResponseEntity.ok(ApiResponse.success("필터 결과에 따른 신규 브랜드 목록 조회가 완료되었습니다.", data));
+    }
+}
