0b7d8c6de427802666d7e0ddc6a75d18cafca840
chamelewan <wani001110@gmail.com>
2026-06-27 21:10:19 +0900

    feat: [BOARD-03] 덱 탐색 브랜드 카드 조회 Controller 구현

--- src/main/java/com/swatching/swatching_be/domain/brand/controller/BrandController.java
+++ src/main/java/com/swatching/swatching_be/domain/brand/controller/BrandController.java
@@ -1,5 +1,6 @@
 package com.swatching.swatching_be.domain.brand.controller;
 
+import com.swatching.swatching_be.domain.brand.dto.BrandDeckResponseDto;
 import com.swatching.swatching_be.domain.brand.dto.BrandResponseDto;
 import com.swatching.swatching_be.domain.brand.service.BrandService;
 import com.swatching.swatching_be.global.common.ApiResponse;
@@ -24,5 +25,12 @@ public class BrandController {
             @RequestParam(required = false) String keywords) {
         List<BrandResponseDto> data = brandService.getBrands(keywords);
         return ResponseEntity.ok(ApiResponse.success("필터 결과에 따른 신규 브랜드 목록 조회가 완료되었습니다.", data));
+    }
+
+    @GetMapping("/brands/deck")
+    public ResponseEntity<ApiResponse<BrandDeckResponseDto>> getBrandDeck(
+            @RequestParam(required = false) String keywords) {
+        BrandDeckResponseDto data = brandService.getBrandDeck(keywords);
+        return ResponseEntity.ok(ApiResponse.success("덱 탐색용 브랜드 카드 목록 조회가 완료되었습니다.", data));
     }
 }
